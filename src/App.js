854ddaa4a577126be27e78e3144e9eb8aa6d473e
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-03-26 19:46:00 +0200

    Loader and Axios added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Hello World</h1>
-                <Weather />
+                <Weather city="New York" />
             </header>
         </div>
     );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,25 @@
 import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
 
-export default function Weather() {
-    return <h2>Hello from Weather</h2>;
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(
+            `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+        );
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=f81614abe2395d5dfecd45b9298041de&units=metric`;
+    axios.get(url).then(handleResponse);
+    return (
+        <Puff
+            height="80"
+            width="80"
+            radius={1}
+            color="#4fa94d"
+            ariaLabel="puff-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+        />
+    );
 }
